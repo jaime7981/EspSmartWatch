b3172d279b82851682dbf0e85d33132870835e56
jaime7981 <jmfernandez1@miaundes.cl>
2023-05-26 22:25:34 -0400

    interrupts removed and inputs into vtask

--- project/main/smart-watch.cpp
+++ project/main/smart-watch.cpp
@@ -7,13 +7,13 @@
 #include <iostream>
 #include <TFT_eSPI.h>
 
-#define LED_PIN     GPIO_NUM_21
 #define BUTTON_PRIMARY_PIN  GPIO_NUM_12
+#define BUTTON_SECONDARY_PIN  GPIO_NUM_14
 #define ROTARY_ENCODER_A GPIO_NUM_25
 #define ROTARY_ENCODER_B GPIO_NUM_26
 
 TFT_eSPI tft = TFT_eSPI();
-xQueueHandle interputQueue;
+// xQueueHandle interputQueue;
 
 int selector = 0;
 bool led_on = false;
@@ -21,9 +21,10 @@ bool led_on = false;
 struct RotaryEncoderValues {
     int a_last_status;
     int b_last_status;
+    int counter;
 };
 
-struct RotaryEncoderValues rotary_encoder_values = {0, 0};
+struct RotaryEncoderValues rotary_encoder_values = {0, 0, 0};
 
 void trigger_action(int selector) {
     switch(selector) {
@@ -35,11 +36,9 @@ void trigger_action(int selector) {
             break;
         case 2:
             printf("turn on led\n");
-            gpio_set_level(LED_PIN, 1);
             break;
         case 3:
             printf("turn of led\n");
-            gpio_set_level(LED_PIN, 0);
             break;
         default:
             printf("selector out of the scope\n");
@@ -61,17 +60,6 @@ void draw_random_circles(TFT_eSPI &tft) {
     }
 }
 
-void change_led_status() {
-    if (led_on) {
-        gpio_set_level(LED_PIN, 0);
-        led_on = false;
-    }
-    else {
-        gpio_set_level(LED_PIN, 1);
-        led_on = true;
-    }
-}
-
 bool is_button_debounced(gpio_num_t debounce_pin) {
     static uint16_t btndbc = 0, lastb = 0;
     
@@ -89,75 +77,77 @@ bool is_button_debounced(gpio_num_t debounce_pin) {
     return false;
 }
 
-void encoder_status(int rot_a, int rot_b) {
+int update_encoder_counter(int parameter_update) {
+    rotary_encoder_values.counter = rotary_encoder_values.counter + parameter_update;
+    return 0;
+}
+
+void set_encoder_counter(int set_value) {
+    rotary_encoder_values.counter = set_value;
+    printf("Encoder counter reseted to -> %d\n", rotary_encoder_values.counter);
+}
+
+int encoder_status(int rot_a, int rot_b) {
     if (rotary_encoder_values.a_last_status == rot_a && rotary_encoder_values.b_last_status == rot_b) {
-        return ;
+        return 0;
     }
 
     if ((rot_a == 0 && rotary_encoder_values.a_last_status == 1) && rot_b == 1) {
         printf("clockwise status a\n");
+        update_encoder_counter(1);
     }
     else if ((rot_a == 0 && rotary_encoder_values.a_last_status == 1) && rot_b == 0) {
         printf("anti clockwise status a\n");
+        update_encoder_counter(-1);
     }
-    
-    // critical section
+
     rotary_encoder_values.a_last_status = rot_a;
     rotary_encoder_values.b_last_status = rot_b;
+    return 1;
 }
 
-static void IRAM_ATTR gpio_interrupt_handler(void *args)
+void inputs_task(void *params)
 {
-    int pinNumber = (int)args;
-    xQueueSendFromISR(interputQueue, &pinNumber, NULL);
-}
-
-void rotary_encoder_task(void *params)
-{
-    int pinNumber, count = 0;
-    while (true)
-    {
-        if (xQueueReceive(interputQueue, &pinNumber, portMAX_DELAY))
-        {
-            //printf("\nGPIO %d was pressed %d times. The state is %d\n", pinNumber, count++, gpio_get_level(ROTARY_ENCODER_A));
-            //printf("debounced encoder A: %d\n", gpio_get_level(ROTARY_ENCODER_A));
-            //printf("debounced encoder B: %d\n", gpio_get_level(ROTARY_ENCODER_B));
-            encoder_status(gpio_get_level(ROTARY_ENCODER_A), gpio_get_level(ROTARY_ENCODER_B));
+    while (1) {
+        if (is_button_debounced(BUTTON_PRIMARY_PIN)) {
+            draw_random_circles(tft);
         }
+        else if(is_button_debounced(BUTTON_SECONDARY_PIN)) {
+            set_encoder_counter(0);
+        }
+        else if(encoder_status(gpio_get_level(ROTARY_ENCODER_A), gpio_get_level(ROTARY_ENCODER_B))) {
+            printf("Encoder Counter -> %d\n\n", rotary_encoder_values.counter);
+        }
+        vTaskDelay(10);
     }
 }
 
 void load_gpio() {
-    // Led Test
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
-
     // Push Button Primary Setup
     gpio_pad_select_gpio(BUTTON_PRIMARY_PIN);
     gpio_set_direction(BUTTON_PRIMARY_PIN, GPIO_MODE_INPUT);
     gpio_pullup_dis(BUTTON_PRIMARY_PIN);
     gpio_pulldown_en(BUTTON_PRIMARY_PIN);
-    gpio_set_intr_type(BUTTON_PRIMARY_PIN, GPIO_INTR_POSEDGE);
+    
+    // Push Button Secondary Setup
+    gpio_pad_select_gpio(BUTTON_SECONDARY_PIN);
+    gpio_set_direction(BUTTON_SECONDARY_PIN, GPIO_MODE_INPUT);
+    gpio_pullup_dis(BUTTON_SECONDARY_PIN);
+    gpio_pulldown_en(BUTTON_SECONDARY_PIN);
 
     // Rotary Encoder Setup Pin A
     gpio_pad_select_gpio(ROTARY_ENCODER_A);
     gpio_set_direction(ROTARY_ENCODER_A, GPIO_MODE_INPUT);
-    gpio_pulldown_dis(ROTARY_ENCODER_A);
-    gpio_pullup_en(ROTARY_ENCODER_A);
-    gpio_set_intr_type(ROTARY_ENCODER_A, GPIO_INTR_ANYEDGE);
+    gpio_pullup_dis(ROTARY_ENCODER_A);
+    gpio_pulldown_en(ROTARY_ENCODER_A);
 
     // Rotary Encoder Setup Pin B
     gpio_pad_select_gpio(ROTARY_ENCODER_B);
     gpio_set_direction(ROTARY_ENCODER_B, GPIO_MODE_INPUT);
-    gpio_pulldown_dis(ROTARY_ENCODER_B);
-    gpio_pullup_en(ROTARY_ENCODER_B);
-    gpio_set_intr_type(ROTARY_ENCODER_B, GPIO_INTR_ANYEDGE);
+    gpio_pullup_dis(ROTARY_ENCODER_B);
+    gpio_pulldown_en(ROTARY_ENCODER_B);
 
-    interputQueue = xQueueCreate(10, sizeof(int));
-    xTaskCreate(rotary_encoder_task, "LED_Control_Task", 2048, NULL, 1, NULL);
-
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(ROTARY_ENCODER_A, gpio_interrupt_handler, (void *)ROTARY_ENCODER_A);
-    // gpio_isr_handler_add(ROTARY_ENCODER_B, gpio_interrupt_handler, (void *)ROTARY_ENCODER_B);
+    xTaskCreate(inputs_task, "INPUTS_TASK", 2048, NULL, 1, NULL);
 }
 
 extern "C" void app_main()
@@ -168,10 +158,4 @@ extern "C" void app_main()
 
     load_gpio();
 
-    while (1) {
-        if (is_button_debounced(BUTTON_PRIMARY_PIN)) {
-            draw_random_circles(tft);
-        }
-        vTaskDelay(1);
-    }
 }
